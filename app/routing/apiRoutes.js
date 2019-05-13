5073ae1bd8eb772dbb5760df722d7d42430e6762
Affiliatethis <chris@onecrookedtree.com>
2019-05-13 19:58:28 -0400

    4th upload.   There is an error Error: ENOENT: no such file or directory, stat, but it is listening.  Still in working on resolving the error.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,20 @@
 var friends = require('../data/friends');
 
-module.exports = function(app) {
-    app.get('/api/friends', function(req,res){
+
+module.exports = function (app) {
+    app.get('/api/friends', function (req, res) {
         res.json(friends);
-});
+    });
 
-app.post('/api/friends', function(req, res){
-    var newFriend = req.body;
-    friends.push(newFriend);
+    app.post('/api/friends', function (req, res) {
+        var newFriend = req.body;
 
-});
+        console.log('newFriend = ' + JSON.stringify(newFriend));
+        // friends.push(newFriend);
+
+    });
+
+
 
 
 
--- server.js
+++ server.js
@@ -3,17 +3,21 @@ var path = require('path');
 
 // Initialize the app and create a port
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3030;
 
 // Set up body parsing and static middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+
+require('../FriendFinder/app/routing/apiRoutes')(app);
+require('../FriendFinder/app/routing/htmlRoutes')(app);
 
 // Start the server on the port
 app.listen(PORT, function () {
     console.log('Listening on PORT: ' + PORT);
+
 });
+
+
