73a3a3c62cb99931c65efb17e2488a0f70d75da9
Affiliatethis <chris@onecrookedtree.com>
2019-05-13 11:23:13 -0400

    3rd update, add some questions and html code to the survey page.  Added express and path requirements

--- app/data/friends.js
+++ app/data/friends.js
@@ -20,7 +20,7 @@ var friends = [
     },
 
     {
-        "name": "Bertha",
+        "name": "Johnny OntheSpot",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
             5,
